2ec24d01929fd826d08f375b020e5873b2e6e532
Kevin Ndung'u <kevgathuku@gmail.com>
2018-03-12 18:56:16 +0300

    Use Provider from react-redux

--- src/index.js
+++ src/index.js
@@ -1,32 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import PropTypes from "prop-types";
+import { Provider } from "react-redux";
 import Counter from "./Counter";
 import TodoApp from "./TodoApp";
 import registerServiceWorker from "./registerServiceWorker";
 import store from "./store";
 import "./index.css";
 
-class Provider extends Component {
-  static childContextTypes = {
-    store: PropTypes.object
-  };
-
-  getChildContext() {
-    return {
-      store: this.props.store
-    };
-  }
-
-  render() {
-    return this.props.children;
-  }
-}
-
 ReactDOM.render(
   <Provider store={store}>
-    <Counter />
-    <TodoApp />
+    <React.Fragment>
+      <Counter />
+      <TodoApp />
+    </React.Fragment>
   </Provider>,
   document.getElementById("root")
 );
